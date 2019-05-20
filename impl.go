bc4591da9396c9218d55e4237d80c7995df4c40b
Naofumi Okamoto <nokamoto.engr@gmail.com>
2019-05-20 14:23:08 +0900

    Implement gRPÇ service (#2)

---
+++ impl.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	pb "github.com/nokamoto/poc-go-zipkin/service"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"math/rand"
+	"time"
+)
+
+type serviceA struct {
+	cli pb.ServiceBClient
+}
+
+func newServiceA() (*serviceA, error) {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+
+	conn, err := grpc.Dial(*addr, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &serviceA{cli: pb.NewServiceBClient(conn)}, nil
+}
+
+func (a *serviceA) Send(ctx context.Context, req *pb.Request) (*pb.Response, error) {
+	res, err := a.cli.Send(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	milli := rand.Int63n(3000)
+	fmt.Printf("start: heavy workload A %d millis\n", milli)
+	time.Sleep(time.Duration(milli) * time.Millisecond)
+
+	if ctx.Err() == context.Canceled {
+		fmt.Println("A: Client cancelled, abandoning.")
+		return nil, status.Error(codes.Canceled, "A: Client cancelled, abandoning.")
+	}
+
+	fmt.Printf("done: heavy workload A %d millis\n", milli)
+
+	return res, nil
+}
+
+type serviceB struct{}
+
+func (*serviceB) Send(ctx context.Context, _ *pb.Request) (*pb.Response, error) {
+	milli := rand.Int63n(6000)
+	fmt.Printf("start: heavy workload B %d millis\n", milli)
+	time.Sleep(time.Duration(milli) * time.Millisecond)
+
+	if ctx.Err() == context.Canceled {
+		fmt.Println("B: Client cancelled, abandoning.")
+		return nil, status.Error(codes.Canceled, "B: Client cancelled, abandoning.")
+	}
+
+	fmt.Printf("done: heavy workload B %d millis\n", milli)
+
+	return &pb.Response{}, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,83 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	pb "github.com/nokamoto/poc-go-zipkin/service"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
+	"net"
+	"time"
+)
 
+var port = flag.Int("port", 9090, "grpc server port")
+var addr = flag.String("addr", "localhost:9090", "grpc client dial addr")
+
+func serve() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		panic(fmt.Sprintf("listen tcp port (%d) - %v", *port, err))
+	}
+
+	fmt.Printf("listen tcp port (%d)\n", *port)
+
+	opts := []grpc.ServerOption{}
+	server := grpc.NewServer(opts...)
+
+	a, err := newServiceA()
+	if err != nil {
+		panic(err)
+	}
+
+	pb.RegisterServiceAServer(server, a)
+	pb.RegisterServiceBServer(server, &serviceB{})
+	reflection.Register(server)
+
+	fmt.Println("ready to serve")
+	err = server.Serve(lis)
+	if err != nil {
+		panic(fmt.Sprintf("serve %v - %v", lis, err))
+	}
+}
+
+func call() {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+
+	conn, err := grpc.Dial(*addr, opts...)
+	if err != nil {
+		panic(fmt.Sprintf("err: %s %v", *addr, err))
+	}
+	defer conn.Close()
+
+	client := pb.NewServiceAClient(conn)
+
+	for {
+		fmt.Println("call")
+		ctx := context.Background()
+		ctx, cancel := context.WithDeadline(ctx, time.Now().Add(time.Duration(3000)*time.Millisecond))
+
+		res, err := client.Send(ctx, &pb.Request{})
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+
+			if grpc.Code(err) == codes.DeadlineExceeded {
+				cancel()
+			}
+		} else {
+			fmt.Printf("rec: %v\n", res)
+		}
+
+		time.Sleep(time.Duration(5000) * time.Millisecond)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	go call()
+
+	serve()
 }
